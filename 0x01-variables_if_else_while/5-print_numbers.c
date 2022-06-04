1979c3193af1ed89047a6178b1bb5cff626356e4
tesmik <tesmik1264@gmail.com>
2022-06-04 08:59:14 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 0;
+while(a < 10)
+{
+printf("%d", i);
+i++;
+}
+printf('\n');
+return (0);
+}
