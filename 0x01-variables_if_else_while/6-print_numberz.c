da75fcff80dd1f2005c516520e5ac8523223ae2a
tesmik <tesmik1264@gmail.com>
2022-06-04 09:23:24 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = '0';
+while (i < '10')
+{
+putchar('i');
+i++;
+}
+putchar('\n');
+return (0);
+}
