db9730cc450bce84fdd0db28179d3168c5a9cb55
tesmik <tesmik1264@gmail.com>
2022-06-04 08:27:06 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char w, e, q;
+e = 'e';
+q = 'q';
+for (w = 'a'; w <= 'z'; w++)
+{
+if (w != e && w != q)
+putchar(low);
+putchar('\n');
+return (0);
+}
