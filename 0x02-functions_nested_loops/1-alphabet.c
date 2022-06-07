4150c577ac981430fdf4781b40c9fb1f4318114e
tesmik <tesmik1264@gmail.com>
2022-06-07 11:57:07 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - prints lower case alphabet
+ *
+ * Return - always 0 (success)
+ */
+void print_alphabet(void)
+{
+	char low;
+	for(low = 'a'; low <= 'z'; low++)
+	{
+		_putchar(low);
+	}
+	_putchar('\n')
+}
