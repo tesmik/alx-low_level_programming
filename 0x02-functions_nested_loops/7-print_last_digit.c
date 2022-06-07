0bfb74f37adfc5ce579e7bdffcf01fffd37d866f
tesmik <tesmik1264@gmail.com>
2022-06-07 14:00:47 -0700

    task 6

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.c"
+/**
+ *print_last_digit - prints last digit
+ *@n: number being tested
+ *Return: always 0 (success)
+ */
+int print_last_digit(int n)
+{
+return (n % 10);
+return (0);
+}
