29d92a4963e264922b26cb154fd4050ca5fea37c
tesmik <tesmik1264@gmail.com>
2022-06-10 12:46:30 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_isupper - prints uppercase letter
+ *@c: letter being tested
+ *Return: always 0.
+ */
+int _isupper(int c)
+{
+	if (c > 65 && c < 90)
+		return (1);
+	else
+		return (0);
+}
