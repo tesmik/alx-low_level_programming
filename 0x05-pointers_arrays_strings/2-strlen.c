0aed33f7df09cfb9825b642fd39e4aa1bb154228
tesmik <tesmik1264@gmail.com>
2022-06-13 11:15:32 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main,h"
+/**
+ * _strlen - returns the length of the string
+ *@s: string being counted
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+int i;
+i = 0;
+while (*s != '\0')
+{
+i++;
+s++;
+}
+return (i);
+}
