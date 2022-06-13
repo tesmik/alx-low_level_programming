b153d3153dca93b432dd44e88226653edb330623
tesmik <tesmik1264@gmail.com>
2022-06-13 10:56:13 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * swap_int - swaps the value of two integers
+ *@a: number being swapped
+ *@b: number being swapped
+ */
+void swap_int(int *a, int *b)
+{
+int c = *a;
+
+*a = *b;
+a = b;
+*b = c;
+}
