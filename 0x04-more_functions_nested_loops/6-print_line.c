afae5c959e45967c9febe495e0fbe8d36731cc0c
tesmik <tesmik1264@gmail.com>
2022-06-14 01:23:26 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_line - prints line
+ *@n: number being testted
+ * Return: always 0.
+ */
+void print_line(int n)
+{
+int i;
+if (n > 0){
+for (i = 0; i < n; i+)
+_putchar('_');
+}
+_putchar('\n');
+}
