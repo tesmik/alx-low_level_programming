2dc0a33aa56163104703b9378e45b40bdcc7f7d5
tesmik <tesmik1264@gmail.com>
2022-06-14 07:02:55 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,16 +8,17 @@ int main(void)
 {
 int j;
 
-for(j=1; j <= n; j++)
+for(j=1; j <= 100; j++)
 {
 if (((j % 3) == 0) && ((j % 5) == 0))
 printf("FizzBuzz ");
 else if ((j % 3) == 0)
-printf("frizz ");
+printf("Fizz ");
 else if ((j % 5) == 0)
-printf("buzz ");
+printf("Buzz ");
 else
 printf("%d ", j);
 }
+printf("\n");
 return (0);
 }
