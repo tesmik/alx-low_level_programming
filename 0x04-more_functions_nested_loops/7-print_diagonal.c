c56909f49e992eb05dbc1ce66f9d1f1f2564bf37
tesmik <tesmik1264@gmail.com>
2022-06-14 01:58:33 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagonal; prints diagonal line
+ * @n: number being tested
+ * Return: always 0.
+ */
+void print_diagonal(int n)
+{
+int k, j;
+if (n > 0){
+for (j = 0; j < n; j++)
+{
+for (k = 0; k <= j; k++)
+{
+if (k == j)
+_putchar('\');
+else
+_putchar(' ');
+}
+_putchar('\n');
+}
+}
+else
+_putchar('\n');
+_putchar('\n');
+}
