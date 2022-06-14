cf88efa870132ea22d485edcd3492c98cbeec689
tesmik <tesmik1264@gmail.com>
2022-06-14 13:51:57 -0700

    task 2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdio.h>
-void positive_or_negative(int i);
-#endif /* MAIN_H */
 #include <stdio.h>
 #include "main.h"
+
 /**
  * main - prints the largest of 3 integers
  *
@@ -14,10 +10,14 @@ int main(void)
 {
 int a, b, c;
 int largest;
+
 a = 972;
 b = -98;
 c = 0;
+
 largest = largest_number(a, b, c);
+
 printf("%d is the largest number\n", largest);
+
 return (0);
 }
