7a37b73f6298510eaef646267321daef27fc4aad
tesmik <tesmik1264@gmail.com>
2022-06-15 12:11:14 -0700

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * leet - Entry point
- * ONE if, TWO loops only...
  * @n: input
  * Return: Always 0 (Success)
  */
@@ -11,12 +10,15 @@ int i, x;
 int find[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 int replacer[] = {'4', '3', '0', '7', '1'};
 
+for (i = 0; n[i] != '\0'; i++)
+{
 for (x = 0; x <= 9; x++)
 {
 if (n[i] == find[x])
 {
 n[i] = replacer[x / 2];
 x = 9;
+}
 }
 }
 return (n);
