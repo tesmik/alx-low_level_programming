5b6555aa8a3559d427f46b55147d922733c54229
tesmik <tesmik1264@gmail.com>
2022-06-16 13:43:51 -0700

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,22 +4,18 @@
  * @n: input
  * Return: Always 0 (Success)
  */
- char *leet(char *n)
+char *leet(char *s)
 {
-int i, x;
-int find[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-int replacer[] = {'4', '3', '0', '7', '1'};
+int i = 0, j;
+char subs[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'},
+str[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 
-for (i = 0; n[i] != '\0'; i++)
+while (s[i])
 {
-for (x = 0; x <= 9; x++)
-{
-if (n[i] == find[x])
-{
-n[i] = replacer[x / 2];
-x = 9;
+for (j = 0; j < 10; j++)
+if (s[i] == str[j])
+s[i] = subs[j];
+i++;
 }
-}
-}
-return (n);
+return (s);
 }
