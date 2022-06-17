bdf238a8a58415254f6dcf900d16103a7092e51e
tesmik <tesmik1264@gmail.com>
2022-06-17 15:50:03 -0700

    task

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,22 +8,22 @@ void print_number(int n)
 {
 if (n >= 1000)
 {
-_putchar(n/1000 +'0');
+_putchar(n/1000 + '0');
 _putchar((n % 1000) / 100 + '0');
 _putchar(((n % 1000) % 100 / 10 ) + '0');
-_putchar(((n % 1000) % 100 % 10) +'0');
+_putchar(((n % 1000) % 100 % 10) + '0');
 }
 else if (n >= 100)
 {
-_putchar(n / 100 +'0');
+_putchar(n / 100 + '0');
 _putchar((n % 100) / 10 + '0');
-_putchar(((n % 100) % 1o) +'0');
+_putchar(((n % 100) % 1o) + '0');
 }
 else if(n >= 10)
 {
-_putchar(n / 10 +'0');
+_putchar(n / 10 + '0');
 _putchar(n % 10+ '0');
 }
 else
-_putchar(n % 10 +'0');
+_putchar(n % 10 + '0');
 }
