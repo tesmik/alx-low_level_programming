b5b7a7b121852f41adae15a135a8d6b2312bbf22
tesmik <tesmik1264@gmail.com>
2022-06-17 15:46:03 -0700

    task

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_number - prints number
+ * @n: number being tested
+ * Return: always 0.
+ */
+void print_number(int n)
+{
+if (n >= 1000)
+{
+_putchar(n/1000 +'0');
+_putchar((n % 1000) / 100 + '0');
+_putchar(((n % 1000) % 100 / 10 ) + '0');
+_putchar(((n % 1000) % 100 % 10) +'0');
+}
+else if (n >= 100)
+{
+_putchar(n / 100 +'0');
+_putchar((n % 100) / 10 + '0');
+_putchar(((n % 100) % 1o) +'0');
+}
+else if(n >= 10)
+{
+_putchar(n / 10 +'0');
+_putchar(n % 10+ '0');
+}
+else
+_putchar(n % 10 +'0');
+}
