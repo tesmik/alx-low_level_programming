ec582be4a016c09f2bdc4df8b4244b61d4c831c0
tesmik <tesmik1264@gmail.com>
2022-06-20 13:45:26 -0700

    tas
    cd alx-low_level_programming
    cd 0x07-pointers_arrays_strings
    vim 8-print_diagsums.c
    vim 8-print_diagsums.c
    git add .
    git add .
    git add .
    git commit -m tas

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "stdio.h"
+/*
+ *print_diagsums - print diagonal sum
+ *@a: square matrix
+ *@size: size of integers
+ *return: non
+ */
+void print_diagsums(int *a, int size)
+{
+int i = 0, mul = size * size, sum1 = 0, sum2 = 0;
+
+for (; i < mul; i += size + 1)
+sum1 += a[i];
+for (i = size - 1; i < mul - 1; i += size - 1)
+sum2 += a[i];
+printf("%d, %d\n", sum1, sum2);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,3 +5,4 @@ char *_strchr(char *s, char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(int n);
+void print_diagsums(int *a, int size);
