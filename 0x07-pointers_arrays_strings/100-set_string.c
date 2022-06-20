98bd2fcf8066ad907ea30f60f78f749ca57eb688
tesmik <tesmik1264@gmail.com>
2022-06-20 13:59:43 -0700

    tas8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ *set_string - set valus of pointer
+ *@s: twice pointer
+ *@to:pointer to string
+ *Return: non
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,3 +6,4 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(int n);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
