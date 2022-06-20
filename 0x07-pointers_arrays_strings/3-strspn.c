0ecd8532f47a273c518a44ef284206947f835846
tesmik <tesmik1264@gmail.com>
2022-06-20 11:43:06 -0700

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -5,14 +5,27 @@
  *@accept: copy from
  *Return: bytes
  */
- unsigned int _strspn(char *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
 {
-int i = 0;
+int count = 0, flag;
+char *start = accept;
 
-while(*s != ',')
+while (*s)
 {
-i++;
-s++;
+flag = 0;
+while (*accept)
+if (*accept == *s)
+{
+count++;
+flag = 1;
+break;
 }
-return (i);
+accept++;
+}
+s++;
+accept = start;
+if (flag == 0)
+break;
+}
+return (count);
 }
