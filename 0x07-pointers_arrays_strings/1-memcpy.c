7b909f8320eeefb30821eb860d8588ad43c8d605
tesmik <tesmik1264@gmail.com>
2022-06-20 08:18:11 -0700

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * _memcpy - prints 
+ * @dest: string tested
+ * @src: string being tested
+ * @n: size of array
+ * Return: always 0.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+dest[i] = src[i];
+dest[i] = '\0';
+return (dest);
+}
