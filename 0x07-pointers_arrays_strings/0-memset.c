dc38cd4398343ad6e6779973a32c383ee6a4d7fd
tesmik <tesmik1264@gmail.com>
2022-06-20 10:49:54 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,10 +9,11 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-int i;
-
-for (i = 0; i < n; i++)
-*(s + i) = b;
-*(s + i) = '\0';
-return(0);
+char *tmp = s;
+while (n--)
+{
+*s = b;
+s++;
+}
+return (tmp);
 }
