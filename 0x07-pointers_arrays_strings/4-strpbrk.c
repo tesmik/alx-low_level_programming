be329a5c15521d9fff03e1958749cdf484c8e733
tesmik <tesmik1264@gmail.com>
2022-06-20 12:59:19 -0700

    task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *_strpbrk - prints the string
+ *@s: copy to
+ *@accept: copy from
+ *Return: retur the string
+ *or NULL if the string is not match.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+char *tmp = accept;
+
+while (*s)
+{
+while (*accept)
+{
+if (*accept == *s)
+return (s);
+accept++;
+}
+accept = tmp;
+s++;
+return (NULL);
+}
