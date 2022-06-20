a364b2be9cd6f4b03a69a512df6cdac537b2009d
tesmik <tesmik1264@gmail.com>
2022-06-20 10:34:48 -0700

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,7 +12,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 int i;
 
 for (i = 0; i < n; i++)
-dest[i] = src[i];
-dest[i] = '\0';
+*(dest + i) = *(src + i);
+*(dest + i) = '\0';
 return (dest);
 }
