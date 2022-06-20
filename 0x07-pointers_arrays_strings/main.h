1d40648daae040c1260f3df2f6345421b0e18fa2
tesmik <tesmik1264@gmail.com>
2022-06-20 13:32:54 -0700

    task 6

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,3 +4,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strchr(char *s, char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memset(char *s, char b, unsigned int n);
+int _putchar(int n);
