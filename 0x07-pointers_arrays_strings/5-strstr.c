dfbd038e9e480861d04c3b382e17f89bb7c9fbbf
tesmik <tesmik1264@gmail.com>
2022-06-20 07:33:15 -0700

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ *_strstr - prints searching string
+ *@haystack: strings being  
+ *@needle: string being tested
+ *Return: always 0.
+ */
 char *_strstr(char *haystack, char *needle)
 {
 char *tmp = haystack;
