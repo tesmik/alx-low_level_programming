2449c258f1350f3f03bcc5009cac3650c7cedea3
tesmik <tesmik1264@gmail.com>
2022-06-22 02:36:31 -0700

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_print_rev_recursion - prints reverse of string
+ *@s:string being processed
+ *Return: always 0.
+ */
+void _print_rev_recursion(char *s)
+{
+if (*s != '\0')
+_print_rev_recursion(s + 1);
+else
+return;
+printf("%c",*s);
+}
