0c1a4ca09d1ff55834e8a6afa098468729bb5953
tesmik <tesmik1264@gmail.com>
2022-06-28 01:55:08 -0700

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,10 +14,10 @@ if (str == NULL)
 return (NULL);
 for (i = 0; str[i]; i++)
 len++;
-duplicate = malloc(sizeof(char) * (len + 1)); 
+duplicate = malloc(sizeof(char) * (len + 1));
 if (duplicate == NULL)
 return (NULL);
-for (i=0; str[i]; i++)
+for (i = 0; str[i]; i++)
 duplicate[i] = str[i];
 duplicate[len] = '\0';
 return (duplicate);
