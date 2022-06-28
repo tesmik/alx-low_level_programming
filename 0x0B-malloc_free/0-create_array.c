cc12cc0508a257e7cf3423ba773efb3953121ea7
tesmik <tesmik1264@gmail.com>
2022-06-28 01:15:45 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ return (NULL);
 array = malloc(sizeof(char) * size);
 if (array == NULL)
 return (NULL);
-for (index=0; index < size; index++)
+for (index = 0; index < size; index++)
 array[index] = c;
 return (array);
 }
