bfb6dddae89b0faeaecdfad3401454d1a20d2b9f
tesmik <tesmik1264@gmail.com>
2022-06-28 01:51:14 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *_strdup - copy the string
+ *@str: string to be copy
+ *Return: 0.
+ */
+char *_strdup(char *str)
+{
+int i, len = 0;
+char *duplicate;
+
+if (str == NULL)
+return (NULL);
+for (i = 0; str[i]; i++)
+len++;
+duplicate = malloc(size_of(char) * len + 1); 
+if (duplicate == NULL)
+return (NULL);
+for (i=0; str[i]; i++)
+duplicate[i] = str[i];
+duplicate[len] = '\0';
+return (duplicate);
+}
